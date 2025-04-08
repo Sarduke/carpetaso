3fcce2c324037e6bbdc076647088bf79c6266fdc
John Doe <johndoe@example.com>
2025-04-08 20:37:38 -0300

    primer cambio

--- carpetazo/routes/web.php
+++ carpetazo/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome to mobiile legends');
+});
+
+Route::get('/hola', function () {
+    return "welcome to mobiile legends";
 });
